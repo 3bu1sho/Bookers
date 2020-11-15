efa6f32119a2cc260af4c3e94a89123361437863
3bu1sho <3bu1sho@gmail.com>
2020-11-15 14:15:14 +0000

    aaa

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,9 +1,19 @@
 class BooksController < ApplicationController
   
     def index
-      @book = Book.all
+      @book = Book.new
+      @books = Book.all
     end
-
+    
+    def create
+    @book = Book.new(book_params)
+    @book.save
+    if @book.save
+    flash[:notice] = "form,successfully"
+    end
+    redirect_to books_path
+    end
+    
     def show
     @book = Book.find(params[:id])
     end
@@ -11,21 +21,27 @@ class BooksController < ApplicationController
     def new
     @book = Book.new
     end
-  
-    def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path
-    end
 
     def edit
     @book = Book.find(params[:id])
     end
     
+    def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.update(book_params)
+    flash[:notice] = "form,successfully"
+    end
+    redirect_to '/books'
+    end
+    
     def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to book_path
+    @book = Book.find(params[:id])
+    @book.destroy
+    if @book.destroy
+    flash[:notice] = "form,successfully"
+    end
+    redirect_to '/books'
     end
   
     private
--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
    root :to => 'books#index'
    
    post 'todolists' => 'todolists#create'
+    
+   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
